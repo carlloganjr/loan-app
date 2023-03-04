a848d5f77737d5740488c3576354f615bdb40367
Carl Logan <carlloganjr@gmail.com>
2023-03-03 21:14:27 -0700

    updated the loan app calculation function

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# InNOutBooksp1
+# Loan App
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.2.4.
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,9 +16,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // capture the input values
+  principle: number;
+  interestRate: number;
+  loanTerm: number;
+
+  // output the calculated values for display
+  monthlyPayment: number = 0;
+  totalInterest: number = 0;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  // calculate the input from the app form
+  calculate() {
+    const monthlyRate = this.interestRate / 100 / 12;
+    const payments = this.loanTerm * 12;
+    const paymentPerPeriod = this.principle *
+    (
+      (monthlyRate * Math.pow(1 + monthlyRate, payments))
+      / (Math.pow(1 + monthlyRate, payments) - 1)
+    );
+    this.monthlyPayment = +paymentPerPeriod.toFixed(2);
+    this.totalInterest = +(paymentPerPeriod * payments - this.principle).toFixed(2);
   }
 
 }
